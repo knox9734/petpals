13e7ce02079e1af56da72eaf3bf5d738086ee920
Chanaka Wijerathna <chanaka.wijerathna@pearson.com>
2026-04-24 09:27:16 +0530

    Removing files

--- infra/providers.tf
+++
@@ -1,9 +0,0 @@
-provider "aws" {
-  region = var.aws_region
-}
-
-# CloudFront requires ACM certificates to be created in us-east-1.
-provider "aws" {
-  alias  = "us_east_1"
-  region = "us-east-1"
-}
